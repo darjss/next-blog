6c6d7a1764be58327576d2e3380c00a1b951eab1
darjss <darjsavid@gmail.com>
2024-02-06 11:55:11 +0800

    2.6 class final

--- src/pages/[id]/index.jsx
+++ src/pages/[id]/index.jsx
@@ -1,20 +1,20 @@
 import parse from "html-react-parser";
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
-const Page = () => {
-  const [news, setNews] = useState([]);
-  useEffect(() => {
-    getData();
-  }, []);
-  const router = useRouter();
-  const { id } = router.query;
-  const getData = async () => {
-    const res = await fetch(`http://localhost:4000/api/articles/${id}`).then(
-      (response) => response.json()
-    );
-    console.log(res);
-    setNews(res);
-  };
+const Page = ({news}) => {
+  // const [news, setNews] = useState([]);
+  // useEffect(() => {
+  //   getData();
+  // }, []);
+  // const router = useRouter();
+  // const { id } = router.query;
+  // const getData = async () => {
+  //   const res = await fetch(`http://localhost:4000/api/articles/${id}`).then(
+  //     (response) => response.json()
+  //   );
+  //   console.log(res);
+  //   setNews(res);
+  // };
   return (
     <div className="w-[1200px] m-auto">
       <p>{news.title}</p>
@@ -28,14 +28,14 @@ const Page = () => {
   );
 };
 export default Page;
-// export async function getServerSideProps(context) {
-//   const { id } = context.query;
-//   const news = await fetch(`https://dev.to/api/articles/${id}`).then(
-//     (response) => response.json()
-//   );
-//   return {
-//     props: {
-//       news,
-//     },
-//   };
-// }
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+  const news = await fetch(`https://dev.to/api/articles/${id}`).then(
+    (response) => response.json()
+  );
+  return {
+    props: {
+      news,
+    },
+  };
+}
